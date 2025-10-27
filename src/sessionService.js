e02d1baac1cd8bd66fd7bc60d6583c39fc4fbbc1
player00001-26 <player012609@gmail.com>
2025-10-28 01:09:41 +0530

    Create sessionService.js

---
+++ src/sessionService.js
@@ -0,0 +1,26 @@
+import { db, ensureAuth } from "./firebaseConfig"
+import { ref, set, get, onValue, remove, push } from "firebase/database"
+
+export async function createSession(data) {
+  await ensureAuth()
+  const newRef = push(ref(db, "sessions"))
+  await set(newRef, {
+    ...data,
+    createdAt: Date.now(),
+  })
+  return newRef.key
+}
+
+export async function listenAllSessions(callback) {
+  await ensureAuth()
+  const sessionsRef = ref(db, "sessions")
+  const unsub = onValue(sessionsRef, (snap) => {
+    callback(snap.val() || {})
+  })
+  return () => unsub()
+}
+
+export async function deleteSession(id) {
+  await ensureAuth()
+  await remove(ref(db, `sessions/${id}`))
+    }
